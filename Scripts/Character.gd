ce7c49c627e03f72ad7d7a80f9f06e598f996fbb
maddiemcm <mabmcmul@iu.edu>
2019-12-12 01:56:30 -0500

    updates

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,33 +1,47 @@
 extends KinematicBody
 
 var gravity = Vector3.DOWN * 12
-var speed = 4
-var jump_speed = 6
-var character
+var speed = 5
+var jump_speed = 5
+var spin = 0.1
+#var character
 
 var velocity = Vector3()
-
-func _ready():
-	character = get_node("Character")
+var jump = false
+#func _ready():
+#	character = get_node("Character")
 	
 func _physics_process(delta):
 	velocity += gravity * delta
 	get_input()
 	velocity = move_and_slide(velocity, Vector3.UP)
-	var angle = atan2(velocity.x, velocity.z)
-	var char_rot = character.get_rotation()
-	char_rot.y = angle
-	character.set_rotation(char_rot)
+	if jump and is_on_floor():
+		velocity.y = jump_speed
+	
+#	if is_moving:
+#		var angle = atan2(velocity.x, velocity.z)
+#		var char_rot = character.get_rotation()
+#		char_rot.y = angle
+#		character.set_rotation(char_rot)
 	
 	
 func get_input():
-	velocity.x = 0
-	velocity.y = 0
+	var vy = velocity.y
+	velocity = Vector3()
 	if Input.is_action_pressed("move_forward"):
-		velocity.z -= speed
+		velocity += -transform.basis.z * speed
 	if Input.is_action_pressed("move_back"):
-		velocity.z += speed
+		velocity += transform.basis.z * speed
 	if Input.is_action_pressed("strafe_right"):
-		velocity.x += speed
+		velocity += transform.basis.x * speed
 	if Input.is_action_pressed("strafe_left"):
-		velocity.x -= speed
+		velocity += -transform.basis.x * speed
+	velocity.y = vy
+	jump = false
+	if Input.is_action_just_pressed("jump"):
+		jump = true
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-lerp(0, spin, event.relative.x/10))
+		
